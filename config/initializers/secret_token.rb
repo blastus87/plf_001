82eea2d3f0f438cbad73dbc873405739c880d83a
Pascal Faber <plfdeluxe@bluemail.ch>
2013-11-08 14:35:28 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plf001::Application.config.secret_key_base = '491379fdb7a2d43c13ad030b065f1ebbf903756b6301ec665abaf8106db15faf37752696f20e8de43881c0e45d3f01e785d667d5c210ff6c71387dbe20464df1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plf001::Application.config.session_store :cookie_store, key: '_plf_001_session'
